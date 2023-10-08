384382f5ec53c1a20ab39b419cb5b4d21e0a9972
plinio-martiny <everton.vipcidades@gmail.com>
2023-10-08 15:54:23 -0300

    commit 1

---
+++ Hero.js
@@ -0,0 +1,31 @@
+class Hero {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque;
+    switch (this.tipo) {
+      case 'mago':
+        ataque = 'magia';
+        break;
+      case 'guerreiro':
+        ataque = 'espada';
+        break;
+      case 'monge':
+        ataque = 'artes marciais';
+        break;
+      case 'ninja':
+        ataque = 'shuriken';
+        break;
+      default:
+        ataque = 'um ataque desconhecido';
+    }
+    console.log(`O ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+const meuHeroi = new Hero('Herói 1', 30, 'mago');
+meuHeroi.atacar();
